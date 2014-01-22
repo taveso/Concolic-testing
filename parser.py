9b0d361693825e5d6adb8e21e5052b899198ab30
Sonny Tavernier <sonny.tavernier@gmail.com>
2014-01-22 11:24:26 -0800

    improved the parser

--- parser.py
+++ parser.py
@@ -2,10 +2,12 @@ from pyparsing import Literal, Word, alphanums, nums, Forward, ZeroOrMore
 import re
 
 var_cnt = 0
-var_constraint_dic = {}
-var_input_dic = {}
-var_size_dic = {}
-valgrind_operations = []
+var_constraint_dic = None
+var_input_dic = None
+var_size_dic = None
+valgrind_operations = None
+var_shift_dic = None
+var_realsize_dic = None
 
 def new_var():
 	global var_cnt
@@ -17,7 +19,7 @@ def resize(oldvar, oldsize, newsize, l):
 	
 	newvar = new_var()
 	var_size_dic[newvar] = newsize
-	l.append(('assign', newvar, oldvar, newsize-oldsize))
+	l.append(('assign', newvar, oldvar, (oldsize,newsize)))
 	
 	return newvar
 
@@ -54,7 +56,7 @@ def add_operation(operation, first_operand, second_operand, dest_operand, constr
 	var_constraint_dic[dest_operand] = constraint
 
 def parse_function(s, loc, toks):
-	global var_constraint_dic, var_input_dic, var_size_dic
+	global var_constraint_dic, var_input_dic, var_size_dic, var_realsize_dic
 	
 	operation = toks[0]
 	string = ''.join(toks)
@@ -63,26 +65,31 @@ def parse_function(s, loc, toks):
 	if m:
 		newvar = new_var()
 		var_constraint_dic[newvar] = string
-		var_input_dic[newvar] = m.group(1)
+		var_input_dic[newvar] = int(m.group(1))
 		return
 		
 	for var, constraint in var_constraint_dic.iteritems():
 		m = re.match('^[a-zA-Z0-9:_]+\(%s\)$'%re.escape(constraint), string)
 		if m:
-			if var not in var_size_dic:
-				m = re.match('^LDle:(\d+)$', operation)
-				if m:
-					var_size_dic[var] = m.group(1)
+			m = re.match('^LDle:(\d+)$', operation)
+			if m:
+				if var not in var_size_dic:
+					var_size_dic[var] = int(m.group(1))
+				var_realsize_dic[var] = int(m.group(1))
 			
 			add_operation(operation, var, None, var, string)			
 			return
 			
-		m = re.match('^[a-zA-Z0-9:]+\(%s,(\d+)\)$'%re.escape(constraint), string)
+		m = re.match('^[a-zA-Z0-9:_]+\(%s,(\d+)\)$'%re.escape(constraint), string)
 		if m:
+			mm = re.match('^Shr\d+_$', operation)
+			if mm:
+				var_shift_dic[var] = int(m.group(1))
+		
 			dest_operand = new_var() if re.match('(Add|Sub|Mul|DivModS\d+to)\d+', operation) else var
 			add_operation(operation, var, m.group(1), dest_operand, string)
 			return		
-		m = re.match('^[a-zA-Z0-9:]+\((\d+),%s\)$'%re.escape(constraint), string)
+		m = re.match('^[a-zA-Z0-9:_]+\((\d+),%s\)$'%re.escape(constraint), string)
 		if m:
 			dest_operand = new_var() if re.match('(Add|Sub|Mul|DivModS\d+to)\d+', operation) else var
 			add_operation(operation, m.group(1), var, dest_operand, string)
@@ -94,9 +101,23 @@ def parse_function(s, loc, toks):
 			if m:
 				add_operation(operation, var1, var2, new_var(), string)
 				return
+				
+def init():
+	global var_cnt, var_constraint_dic, var_input_dic, var_size_dic
+	global valgrind_operations, var_shift_dic, var_realsize_dic
+	
+	var_cnt = 0
+	var_constraint_dic = {}
+	var_input_dic = {}
+	var_size_dic = {}
+	valgrind_operations = []
+	var_shift_dic = {}
+	var_realsize_dic = {}
 	
 def parse_constraint(constraint):
-	global var_input_dic, var_size_dic, valgrind_operations
+	global var_realsize_dic, var_shift_dic, var_input_dic, var_size_dic, valgrind_operations
+	
+	init()
 
 	lparen = Literal("(")
 	rparen = Literal(")")
@@ -116,4 +137,4 @@ def parse_constraint(constraint):
 	
 	check_operands_size()
 	
-	return (var_input_dic, var_size_dic, valgrind_operations)
+	return (var_realsize_dic, var_shift_dic, var_input_dic, var_size_dic, valgrind_operations)
